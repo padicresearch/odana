c7cdca1f510920333212ba50c0c6a64336514fc8
mambisi <lilbizi96@gmail.com>
2021-08-08 07:38:36 +0000

    Blockchain

--- node/src/blockchain.rs
+++ node/src/blockchain.rs
@@ -3,10 +3,29 @@ use merkle::MerkleRoot;
 
 type BlockHash = [u8; BLOCK_HASH_LEN];
 type BlockState = [u8; BLOCK_HASH_LEN];
+type Balance = u128;
+type Address = [u8; 64];
+type PubKey = [u8; 32];
+type Sig = [u8; 32];
 
-struct Block {
+
+struct BlockHeader {
     prev_hash: BlockHash,
     hash : BlockHash,
     merkle_root : MerkleRoot,
     state : BlockState
 }
+
+struct BlockTransaction {
+    amount : Balance,
+    from : Address,
+    to : Address,
+    pub_key : PubKey,
+    sig : Sig
+}
+
+struct Block {
+    header : BlockHeader,
+    txs : Vec<BlockTransaction>
+}
+
