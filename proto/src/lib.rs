494660c4b01bce02d6f73ca7c8c6a3c8855fc3fb
mambisizempare <lilbizi96@gmail.com>
2022-08-04 05:35:05 +0000

    remove debug messages

--- proto/src/lib.rs
+++ proto/src/lib.rs
@@ -1,4 +1,6 @@
+#[rustfmt_skip]  // requires nightly and #![feature(custom_attribute)] in crate root
 pub mod rpc;
+#[rustfmt_skip]  // requires nightly and #![feature(custom_attribute)] in crate root
 mod types;
 
 pub use types::*;
