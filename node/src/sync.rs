19aa5ed8926bcc7f71b54f3557578f4391adb228
mambisizempare <lilbizi96@gmail.com>
2022-08-08 22:53:02 +0000

    debug sync failure

--- node/src/sync.rs
+++ node/src/sync.rs
@@ -192,7 +192,8 @@ impl Handler<PeerMessage> for SyncService {
         match self.handle_remote_message(msg.clone()) {
             Ok(_) => {}
             Err(error) => {
-                warn!(target: "sync", error = ?error, msg = format!("{:#?}", msg), "failed to handle remote message");
+                warn!(target: "sync", error = ?error, "failed to handle remote message");
+                println!("{:#?}", msg)
             }
         }
     }
